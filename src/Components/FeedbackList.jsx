05e7ed706876f55e789f308a3856775c546e799c
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-05-11 18:41:17 +1000

    aded PropType and settup shared card component

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
-function FeedbackList() {
+function FeedbackList({feedback}) {
+  if(!feedback || feedback.length === 0){
+    return <p>No Feedback yet</p>
+  }
   return (
-    <div>
-    List
+    <div className='feedback-list'>
+   {feedback.map((item) => (
+  <FeedbackItem key={item.id} item={item} />
+  ))}
     </div>
-  )
+  )}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired
+        })
+    )
 }
 
 export default FeedbackList
