3b7b2c2f4e653c02f18a3a6714e474cb4f32932b
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-05-11 17:30:45 +1000

    added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App(){
-    return (
-        <h1> Hello from the App componenet</h1>
-    )
+import React from "react";
+import Header from "./Components/Header";
+
+function App() {
+  
+
+  return (
+    <>
+        <Header/>
+        <div className= 'container'>
+             <h1>My App</h1>
+        </div>
+    </>
+  );
 }
 
-export default App
+export default App;
